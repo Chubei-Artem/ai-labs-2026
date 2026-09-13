3f16c67d536e1c439fd4323d96baf68f45bf2f59
Chubei-Artem <chubei.artem@chnu.edu.ua>
2026-09-13 22:23:35 +0300

    Delete task_2 directory

--- task_2/app.py
+++
@@ -1,9 +0,0 @@
-import io
-import numpy as np
-from PIL import Image, ImageDraw
-import streamlit as st
-
-st.set_page_config(
-    page_title="Лабораторна -  Створення адаптивної системи розпізнавання з побудовою кластерів методом статистичної обробки навчальних послідовностей", layout="wide"
-)
-st.title("Завдання 1: Побудова абсолютного та нормованого векторів ознак")
